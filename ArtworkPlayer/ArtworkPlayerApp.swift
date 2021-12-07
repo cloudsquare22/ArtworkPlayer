b25fc43d87773c976afc31ba4e55e2f0216a33b3
shi-n <shi-n_xyz@cloudsquare.jp>
2021-12-07 20:06:05 +0900

    Initial Commit

---
+++ ArtworkPlayer/ArtworkPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtworkPlayerApp.swift
+//  ArtworkPlayer
+//
+//  Created by Shin Inaba on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct ArtworkPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
